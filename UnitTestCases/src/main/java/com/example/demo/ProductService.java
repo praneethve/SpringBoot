eb3afdae4d45e4f3eb1d3b132c3864e6717e8645
PRANEETH <PRANEETH@Pravalli-PC>
2021-03-20 06:49:04 +0530

    unit test case

---
+++ UnitTestCases/src/main/java/com/example/demo/ProductService.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+   public String getProductName() {
+      return "Honey";
+   } 
+}
